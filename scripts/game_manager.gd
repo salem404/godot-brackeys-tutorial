b5938d2e13cd2714da6287f731bc9e7d868b3b28
salem404 <prumeu@protonmail.com>
2025-10-06 21:36:02 +0200

    feat: Added pause functionality

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,9 +2,28 @@ extends Node
 
 
 var score = 0
+var game_running: bool = true
 
 @onready var score_label: Label = $ScoreLabel
 
 func add_point():
-	score+= 1
+	score += 1
 	score_label.text = str(score) + " de 7 monedas"
+
+
+func start_game():
+	score = 0
+	game_running = true
+
+func pause_game():
+	if game_running && Input.is_action_just_pressed("pause"):
+		$Hud/PauseMenu.show()
+		Engine.time_scale = 0
+		game_running = false
+	elif !game_running && Input.is_action_just_pressed("pause"):
+		$Hud/PauseMenu.hide()
+		Engine.time_scale = 1
+		game_running = true
+
+func _process(_delta: float) -> void:
+	pause_game()
